7585da76c7ee873b208e3421d45f656cffddbc5d
KateThePrincess <k99rzonca@gmail.com>
2024-04-26 18:10:50 +0200

    bottom-0 removed

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -3,7 +3,7 @@ import ProductsGrid from './ProductsGrid';
 
 const FeaturedProducts = () => {
   return (
-    <div className='absolute w-[100vw] left-[50%] bottom-0 translate-x-[-50%] py-10 bg-base-200 mt-24'>
+    <div className='absolute w-[100vw] left-[50%] translate-x-[-50%] py-10 bg-base-200 mt-24'>
       <div className='align-element'>
         <SectionTitle text='featured products' />
         <ProductsGrid bg={'bg-base-100'} to='products/' />
